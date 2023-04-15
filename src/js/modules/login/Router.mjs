2b85a329c3c7ea18258661db4d7e4c3535a352d2
Yehor Potebenko <hitechnic.uu68@gmail.com>
2023-04-15 23:25:09 +0200

    feat: add router mock

---
+++ src/js/modules/login/Router.mjs
@@ -0,0 +1,86 @@
+const requestEndpoints = {
+  root: '/',
+  login: '/login',
+  registration: '/registration',
+};
+
+const DEFAULT_PATHNAME = requestEndpoints.login;
+
+export default class Router {
+  static renderContent() {
+    const curentPathname = window.location.pathname;
+
+    switch (curentPathname) {
+      case requestEndpoints.root:
+      case requestEndpoints.registration:
+        console.log('This is welcome or registration page!');
+        break;
+
+      case requestEndpoints.login:
+        console.log('This is login page!');
+        break;
+
+      default:
+        console.log('404 not found');
+    }
+  }
+
+  static handlePageState(event) {
+    event.preventDefault();
+    const targetURL = event.target.getAttribute('href');
+
+    if (targetURL !== window.location.pathname) {
+      window.history.pushState(
+        {
+          requestEndpoint: targetURL,
+        },
+        '',
+        targetURL
+      );
+
+      this.renderContent();
+    }
+  }
+
+  static triggerOnPageLoad() {
+    window.addEventListener('beforeunload', () => {
+      localStorage.setItem('unload', Date.now());
+    });
+
+    const DECIDED_TIME = 1000;
+
+    const lastUnload = localStorage.getItem('unload');
+    const lastUnloadTimestamp = parseInt(lastUnload, 10);
+
+    if (lastUnload)
+      if (Date.now() - lastUnloadTimestamp <= DECIDED_TIME) {
+        let currentPathname = DEFAULT_PATHNAME;
+
+        if (
+          window.location.pathname === requestEndpoints.root ||
+          window.location.pathname === requestEndpoints.login
+        )
+          currentPathname = DEFAULT_PATHNAME;
+        else currentPathname = window.location.pathname;
+
+        window.history.replaceState(
+          {
+            requestEndpoint: currentPathname,
+          },
+          '',
+          currentPathname
+        );
+
+        this.renderContent();
+      } else {
+        window.history.replaceState(
+          {
+            requestEndpoint: requestEndpoints.login,
+          },
+          '',
+          requestEndpoints.login
+        );
+        this.renderContent();
+      }
+  }
+}
--- src/pages/login/script.mjs
+++ src/pages/login/script.mjs
@@ -1,2 +1,13 @@
 import './index.html';
 import './style.scss';
+import Router from '../../js/modules/login/Router.mjs';
+
+const loginSwitcher = document.querySelector('.login__account-join');
+
+window.addEventListener('load', () => {
+  Router.triggerOnPageLoad();
+});
+
+loginSwitcher.addEventListener('click', (e) => {
+  Router.handlePageState(e);
+});
