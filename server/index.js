bfb6cf594843bdd27bc1fd561454d68c6e3e3919
Yehor Potebenko <hitechnic.uu68@gmail.com>
2023-04-18 12:48:28 +0200

    refctor: pull server folder from login-page-validation branch

---
+++ server/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/extensions */
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoute from './routes/auth.js';
+
+const app = express();
+dotenv.config();
+
+// * Constants
+const { PORT } = process.env || 3000;
+const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+// * Middleware
+app.use(cors());
+app.use(express.json());
+
+// * Routes
+app.use('/api/auth', authRoute);
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ooena3i.mongodb.net/${DB_NAME}`
+    );
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
